70c87fdeccb5e6d75762a370db179df12585ff2b
ashwani2797 <ashwani.kumar@nobroker.in>
2020-05-04 00:26:38 +0530

    Added util classes

---
+++ src/views/Quiz/utils/fullScreenUtil.jsx
@@ -0,0 +1,27 @@
+export const fullScreenApi = (element) => {
+    if (element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if (element.mozRequestFullScreen) {
+        element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullscreen) {
+        element.webkitRequestFullscreen();
+    } else if (element.msRequestFullscreen) {
+        element.msRequestFullscreen();
+    }
+};
+
+
+
+export const exitFullscreen = () => {
+    if(document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if(document.mozCancelFullScreen) {
+      document.mozCancelFullScreen();
+    } else if(document.webkitExitFullscreen) {
+      document.webkitExitFullscreen();
+    }
+  }
+
+
+
+  
